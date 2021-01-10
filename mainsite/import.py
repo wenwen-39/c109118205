3ac03453adb8a45a4023078433a726833e1e54d3
wenwen-39 <c109118205@nkust.edu.tw>
2021-01-10 22:26:43 +0800

    first commit

--- mainsite/import.py
+++ mainsite/import.py
@@ -1,11 +1,11 @@
 import csv,sys,os
-project_dir = 'C:\wenweb\mblog'
+project_dir = './mainsite/'
 sys.path.append(project_dir)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'mblog.settings'
 import django
 django.setup()
 from mainsite.models import heightdata
-data = csv.reader(open("C:\wenweb\mblog\mainsite\data.csv",encoding="utf-8"),delimiter=",",)
+data = csv.reader(open("./mainsite/data.csv",encoding="utf-8"),delimiter=",",)
 for row in data:
     if row[0] !='year':
         unit = heightdata()
